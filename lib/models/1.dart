e8825c34d2c1eddd826dcf1ed1e726f84cc3b289
carlstep <carlstep333@me.com>
2023-09-04 11:35:41 +0700

    1.json code

---
+++ lib/models/1.dart
@@ -0,0 +1,20 @@
+// ignore: file_names
+class One {
+  int id;
+  String name;
+  String gender;
+
+  One({
+    required this.id,
+    required this.name,
+    required this.gender,
+  });
+
+  factory One.fromJson(Map<String, dynamic> json) {
+    return One(
+      id: json['id'],
+      name: json['name'],
+      gender: json['gender'],
+    );
+  }
+}
